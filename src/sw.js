55e79f4f7521406e59471ea9abd23a5db12fac7d
Degtyaruk Andrey <hlogeon1@gmail.com>
2024-06-05 02:57:14 +0300

    updates

---
+++ src/sw.js
@@ -0,0 +1,69 @@
+const CACHE_NAME = "curing-calculator-cache";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/src/styles.css",
+  "/src/script.js",
+  "/src/logo_meat192.png",
+  "/src/logo_meat512.png",
+  "/src/logo.png",
+  "/src/formio.full.min.js",
+];
+
+self.addEventListener("install", function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+
+      // Clone the request
+      const fetchRequest = event.request.clone();
+
+      // Try fetching from the network
+      return fetch(fetchRequest).then(function (response) {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+
+        // Clone the response
+        const responseToCache = response.clone();
+
+        // Cache the fetched response
+        caches.open(CACHE_NAME).then(function (cache) {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  const cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
